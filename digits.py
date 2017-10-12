eecf17223776930a584a9f94d637024d4243585b
svladimirov <svladimirov@example.com>
2017-10-12 11:00:55 +1000

    under_score function names

--- digits.py
+++ digits.py
@@ -1,11 +1,11 @@
 ###
 ### Задание 1, Вариант 2, Числа №2
 ###
-def NeededDesk(numbers):
+def needed_desk(numbers):
     desk=0
     for num in numbers:
         desk += int(num) // 2 + int(num) % 2
     return str(desk);
 input=input("Кол-во учеников в каждом классе через пробел: ")
 
-print("Для учеников потребуется: "+ NeededDesk(input.split( ))+" новых парт")
+print("Для учеников потребуется: "+ needed_desk(input.split( ))+" новых парт")
--- strings.py
+++ strings.py
@@ -2,17 +2,17 @@
 ### Задание 1, Вариант 2, Строки №6
 ###
 #Функция подсчета уникальных букв в строке
-def isPangramma(str):
+def is_pangramma(str):
     if len(str) == 0: #Если строка в рекурсии закончилась, выходим из рекурсии
         return 0
     else:
         if str[0] in str[1:]: #Проверяем содержится ли первая буква в строке, в оставшейся части строки
-            return 0 + isPangramma(str[1:]) #Cодержится, возвращаем ноль, отдаем на проверку оставшиеся буквы
+            return 0 + is_pangramma(str[1:]) #Cодержится, возвращаем ноль, отдаем на проверку оставшиеся буквы
         else:
-            return 1 + isPangramma(str[1:]) #Буква не содержится, вовращаем 1 и передаем остаток
+            return 1 + is_pangramma(str[1:]) #Буква не содержится, вовращаем 1 и передаем остаток
 
 #Функция поиска букв в строке, из плюсов не требует ничего делать с пробелами и лишними символами
-def isPangramma_ascii(str):
+def is_pangramma_ascii(str):
     if len(str) >= 26:
         for i in range(26):
             if chr(ord('a') + i) not in str:
@@ -27,15 +27,16 @@ def isPangramma_ascii(str):
 #
 ## Подготовка данных
 #
-input="qwerty   uiopa  sdfghjklzxcv   bnm  "  #вводимая строка
+#вводимая строка
+input=""
 string='' # Объявляка (без нее не захотела, надо разобраться.
 # Сплитм строку по пробелам, потом клеим в строку
 for part in input.split( ):
     string += part
 print (" Вариант Рекурсия:")
-if isPangramma(string.lower()) == 26:
+if is_pangramma(string.lower()) == 26:
     print ("  Введенная строка:\n   "+string+"\n   является панграммой.")
 else:
     print("  Введенная строка:\n   " + string + "\n   не является панграммой, т.к. не уникальных хватает букв, а больше 26 их быть не может по условию")
 print (" Вариант ASCII:")
-print(isPangramma_ascii(input.lower()))
+print(is_pangramma_ascii(input.lower()))
