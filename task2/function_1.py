25ede6fe04c87272a359f2b5057863e545620fc5
svladimirov <svladimirov@example.com>
2017-10-20 11:30:38 +1000

    задача функции 1, Вариант 1

--- task2/function_1.py
+++ task2/function_1.py
@@ -3,3 +3,11 @@
 Напишите функцию distance(x1, y1, x2, y2), вычисляющая расстояние между точкой (x1,y1) и (x2,y2).
 Считайте четыре действительных числа и выведите результат работы этой функции.
 '''
+from math import sqrt
+def distance(x1, y1, x2, y2):
+    a=x1**2-x2**2
+    b=y1**2-y2**2
+    dist=sqrt((abs(x1**2-x2**2))+(abs(y1**2-y2**2)))
+    return dist
+x1,y1,x2,y2 = map(float, input("Введите x1 y1 x2 y2, через пробел: ").split())
+print("%.4f" % distance(x1,y1,x2,y2))
