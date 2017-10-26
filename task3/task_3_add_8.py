c1d9ff8e9fbc3fb93992f384455b4e1105fa87ac
svladimirov <ivc.vladimirovsa@gmail.com>
2017-10-27 09:25:47 +1000

    ls tree + Exceptions

--- task3/task_3_add_8.py
+++ task3/task_3_add_8.py
@@ -1,34 +1,40 @@
 """
-Cоздать типизированный файл записей со сведениями о телефонах абонентов
-каждая запись имеет поля:
-фамилия абонента, год установки телефона, номер телефона.
-По заданной фамилии абонента выдать номера его телефонов.
-Определить количество установленных телефонов с N-го года.
-
 *Для самых смелых.
 Написать консольную утилиту, передав которому путь к какой то папке,
 она выводит список файлов и папок, которые есть в этой папке.
 Используя библиотеки os и sys
 """
-#import os
-#from os.path import isfile, join
+DIRNAME="./test"
+
 from os import listdir
-#mypath = os.path.dirname(os.path.realpath(__file__))
+from os.path import isfile, join
 
 
-def ls_directory(path):
-    return listdir(path)
+def ls_directory(path,move):
+    try:
+        dirs=listdir(path)
+    except NotADirectoryError:
+        print("Данный путь - не директория.")
+        import sys
+        sys.exit(0)
+    except FileNotFoundError:
+        print("Такой директории не существует")
+        import sys
+        sys.exit(0)
+    for idx,obj in enumerate(dirs):
+        stage = "|"
+        if idx == len(dirs)-1:
+            stage = "`"
+        if isfile(join(path, obj)):
+            print("{}{}--{}".format(move, stage, obj))
+        else:
+            print("{}{}--{}".format(move, stage, obj))
+            ls_directory(join(path, obj), move+"|   ")
 
 
-def ls_files_only(path):
-    from os.path import isfile, join
-    return [f for f in listdir(path) if isfile(join(path, f))]
+print(DIRNAME)
+ls_directory(DIRNAME,"")
 
-
-#onlyfiles = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
-#import os
-print (ls_directory('.'))
-print (ls_files_only('.'))
 
 
 #NotADirectoryError
