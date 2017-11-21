1a87f368c0e61e3622913974e331d548662b8247
svladimirov <ivc.vladimirovsa@gmail.com>
2017-11-21 10:26:52 +1000

    refactor #18

--- 2_task_class/couner.py
+++ 2_task_class/couner.py
@@ -13,6 +13,15 @@ class Counter:
     Счетчик может ходит вниз и забираться вверх.
     position - текущая позиция в счетчике.
     """
+
+    @staticmethod
+    def decrease():
+        return 'decrease'
+
+    @staticmethod
+    def increase():
+        return 'increase'
+
     def __init__(self,first=0,last=10,enter=0):
         """
         Инифицализация
@@ -21,42 +30,61 @@ class Counter:
         :param enter: Позиция входа в счетчик
         """
         self.begin, self.end, self.position = first, last, enter
+        self.MODE = 'increase'
 
     def __next__(self):
-        if self.position == self.end:
-            self.position = self.begin
-        else:
-            self.position += 1
-        return self.position
+        if self.MODE == 'increase':
+            if self.position == self.end:
+                self.position = self.begin
+            else:
+                self.position += 1
+            return self.position
+        if self.MODE == 'decrease':
+            if self.position == self.begin:
+                self.position = self.end
+            else:
+                self.position -= 1
+            return self.position
 
-    def prev(self):
-        if self.position == self.begin:
-            self.position = self.end
-        else:
-            self.position -= 1
-        return self.position
 
-print("Create Counter with default init.")
+print("Демонстрация возможностей класса:\n")
+print("Инициализируем счетчик по умолчанию: \ncount = Counter()")
 count = Counter()
-print("Position of counter: {}".format(count.position))
+print("Проверяем начальную позицию счетчика: {}".format(count.position))
+# Запоминаем позицию, для расчета отклонения.
+diff = count.position
 up = 4
+print("Вызываем последовательно next(count) {} раза.".format(up))
 for _ in range(up):
     next(count)
-print("Position of counter after increase of {1}: {0}".format(count.position,up))
+print("После выхова next(count), ожидаем позицию счетчика {1}, а она: {0}".format(count.position, diff + up))
+print("Меняем режим счетчика на уменьшение: \ncount.MODE = Counter.decrease()")
+count.MODE = Counter.decrease()
 down = 17
+# Запоминаем позицию, для расчета отклонения.
+diff = count.position
+print("Вызываем последовательно next(count) {} раза. В режиме уменьшения".format(down))
 for _ in range(down):
-    count.prev()
-print("Position of counter after decrease of {1}: {0}\n".format(count.position, down))
-
-s,e,p = (5,200,67)
-print("Create Counter with custom params. Start in {}, End in {}, Start position is {}".format(s,e,p))
-count = Counter(first=s,last=e,enter=p)
-print("Position of counter: {}".format(count.position))
+    next(count)
+print("После выхова next(count), ожидаем позицию счетчика {1}, а она: {0}, правильно, границы {2}:{3} и счетчик считает по кругу".format(count.position, diff - down, count.begin, count.end))
+print("\n")
+start_counter, end_couter, position_counter = 5, 200, 67
+print("Инициализируем счетчик по значениям: \ncount = Counter({}, {}, {})".format(start_counter, end_couter, position_counter))
+count = Counter(start_counter, end_couter, position_counter)
+print("Проверяем начальную позицию счетчика: {}".format(count.position))
+# Запоминаем позицию, для расчета отклонения.
+diff = count.position
 up = 27
+print("Вызываем последовательно next(count) {} раза.".format(up))
 for _ in range(up):
     next(count)
-print("Position of counter after increase of {1}: {0}".format(count.position,up))
+print("После выхова next(count), ожидаем позицию счетчика {1}, а она: {0}".format(count.position, diff + up))
+print("Меняем режим счетчика на уменьшение: \ncount.MODE = Counter.decrease()")
+count.MODE = Counter.decrease()
 down = 92
+# Запоминаем позицию, для расчета отклонения.
+diff = count.position
+print("Вызываем последовательно next(count) {} раза. В режиме уменьшения".format(down))
 for _ in range(down):
-    count.prev()
-print("Position of counter after decrease of {1}: {0}".format(count.position, down))
+    next(count)
+print("После выхова next(count), ожидаем позицию счетчика {1}, а она: {0}, правильно, границы {2}:{3} и счетчик считает по кругу".format(count.position, diff - down, count.begin, count.end))
